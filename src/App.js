0c2da0a4035332d5ad4d2be1bf26149f89977f2b
DerekG29 <derekgrubbs29@gmail.com>
2024-03-06 20:41:56 -0500

    main tasks complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -10,10 +13,9 @@ export default function App () {
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
+        .get('http://localhost:5001/api/movies')
         .then(response => {
-          // Study this response with a breakpoint or log statements
-          // and set the response data as the 'movies' slice of state
+          setMovies(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +32,10 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Routes>
+        <Route path='/' element={<MovieList movies={movies} />} />
+        <Route path='movies/:id' element={<Movie />} />
+      </Routes>
     </div>
   );
 }
--- src/Movies/MovieList.js
+++ src/Movies/MovieList.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+
+
 
 export default function MovieList(props) {
+  const navigate = useNavigate();
+  const goToMovie = id => () => {
+    navigate(`movies/${id}`)
+  }
+
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails
+          link={<Link to={`movies/${movie.id}`}>Details</Link>}
+          goToMovie={goToMovie}
+          key={movie.id}
+          movie={movie}
+        />
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+  const { goToMovie, link } = props;
 
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={goToMovie(id)}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -22,6 +36,7 @@ function MovieDetails(props) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+      {link}
     </div>
   );
 }
